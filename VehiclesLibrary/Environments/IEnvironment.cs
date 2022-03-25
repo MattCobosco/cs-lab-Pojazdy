61a1639f073d9fb7045436b1eda1e8bdc41eb036
MattCobosco <mateuszkalucki@gmail.com>
2022-03-25 17:10:28 +0100

    code cleanup

--- VehiclesLibrary/Environments/IEnvironment.cs
+++ VehiclesLibrary/Environments/IEnvironment.cs
@@ -16,19 +16,19 @@
             Mps
         }
 
-        public static class Environments
-        {
-            private static LandEnvironment _landEnvironment = new LandEnvironment();
-            private static WaterEnvironment _waterEnvironment = new WaterEnvironment();
-            private static AirEnvironment _airEnvironment = new AirEnvironment();
-            public static LandEnvironment LandEnvironment = _landEnvironment;
-            public static WaterEnvironment WaterEnvironment = _waterEnvironment;
-            public static AirEnvironment AirEnvironment = _airEnvironment;
-        }
-
         public EnvironmentType Type { get; }
         public SpeedUnit Unit { get; }
         public int MinSpeed { get; }
         public int MaxSpeed { get; }
+
+        public static class Environments
+        {
+            private static readonly LandEnvironment _landEnvironment = new LandEnvironment();
+            private static readonly WaterEnvironment _waterEnvironment = new WaterEnvironment();
+            private static readonly AirEnvironment _airEnvironment = new AirEnvironment();
+            public static readonly LandEnvironment LandEnvironment = _landEnvironment;
+            public static readonly WaterEnvironment WaterEnvironment = _waterEnvironment;
+            public static readonly AirEnvironment AirEnvironment = _airEnvironment;
+        }
     }
 }
