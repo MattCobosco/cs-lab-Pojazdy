b491f3fa6b5e35c74f1776e0bea91699daf646ec
MattCobosco <mateuszkalucki@gmail.com>
2022-03-25 16:08:05 +0100

    AirVehicle.cs improved => mps as speed unit, simplified methods

--- VehiclesLibrary/Vehicles/AirVehicle.cs
+++ VehiclesLibrary/Vehicles/AirVehicle.cs
@@ -4,95 +4,118 @@ namespace ClassLibrary.Vehicles
 {
     public class AirVehicle : IVehicle
     {
+        // its default environment is AirEnvironment
+        private static readonly IEnvironment NativeEnvironment = IEnvironment.Environments.AirEnvironment;
+
         public AirVehicle(int horsepower, bool hasEngine, IVehicle.FuelType fuelType)
         {
             Horsepower = horsepower;
             HasEngine = hasEngine;
             FuelType = fuelType;
-            // air vehicle spawns on the ground
-            CurrentEnvironment = IEnvironment.Environments.LandEnvironment;
-            // air vehicle spawns as stationary
-            State = IVehicle.VehicleState.Stationary;
         }
 
+        // native speed unit for AirVehicles is meters per second, all requested speed changes are made in mps
         private double Speed { get; set; }
-        private static IEnvironment CurrentEnvironment { get; set; }
+
+        // AirVehicle spawns on the ground
+        private static IEnvironment CurrentEnvironment { get; set; } = IEnvironment.Environments.LandEnvironment;
         private IVehicle.FuelType FuelType { get; }
+
         public int Horsepower { get; }
-        public IVehicle.VehicleState State { get; private set; }
+
+        // AirVehicle spawns as stationary
+        public IVehicle.VehicleState State { get; private set; } = IVehicle.VehicleState.Stationary;
         public bool HasEngine { get; }
 
         public void Start()
         {
+            var vehicle = (IVehicle) this;
             // if plane is already moving it cannot be started
             if (State == IVehicle.VehicleState.Moving) return;
-            Speed = IEnvironment.Environments.LandEnvironment.MinSpeed;
+            Speed = vehicle.GetConvertedSpeed(IEnvironment.SpeedUnit.Kph, IEnvironment.SpeedUnit.Mps,
+                CurrentEnvironment.MinSpeed);
             State = IVehicle.VehicleState.Moving;
         }
-        
+
         public void Stop()
         {
             // if plane is in the air it cannot be stopped
             if (CurrentEnvironment == IEnvironment.Environments.AirEnvironment) return;
-
-            // if plane is already stopped on the ground it cannot be stopped again
-            if (State != IVehicle.VehicleState.Moving) return;
             Speed = 0;
             State = IVehicle.VehicleState.Stationary;
         }
-        // TODO: Fix AirVehicle ignoring min and max speed boundaries while changing environments
-        public void IncreaseSpeed(double change)
+
+        public void IncreaseSpeed(double changeInMps)
         {
-            // if vehicle is in air => check max speed limit and increase speed
+            // increasing speed in air
             if (CurrentEnvironment == IEnvironment.Environments.AirEnvironment)
             {
-                if ((Speed + change) * 3.6 <= IEnvironment.Environments.AirEnvironment.MaxSpeed) Speed += change;
-                else Speed = IEnvironment.Environments.AirEnvironment.MaxSpeed; 
+                // if the requested speed change is within the max speed limit of the AirEnvironment then it's implemented
+                if (Speed + changeInMps <= IEnvironment.Environments.AirEnvironment.MaxSpeed)
+                {
+                    Speed += changeInMps;
+                    return;
+                }
+
+                // if it exceeds the max limit then the max is the new speed
+                Speed = CurrentEnvironment.MaxSpeed;
                 return;
             }
 
-            Speed += change;
-            // Takeoff procedure
-            // if the speed is not enough to take off => return
-            if (!(Speed * 3.6 >= IEnvironment.Environments.AirEnvironment.MinSpeed)) return;
-            CurrentEnvironment = IEnvironment.Environments.AirEnvironment; // takeoff
+            // increasing speed on land
+            Speed += changeInMps;
 
-            if (Speed > 0) State = IVehicle.VehicleState.Moving;
+            if (Speed > IEnvironment.Environments.AirEnvironment.MinSpeed)
+                //takeoff procedure
+                CurrentEnvironment = IEnvironment.Environments.AirEnvironment;
+            // check if requested speed does not exceed the max speed of AirEnvironment
+            if (Speed > CurrentEnvironment.MaxSpeed)
+                //if so, reduce it to max speed
+                Speed = CurrentEnvironment.MaxSpeed;
+            // for situation when air vehicle skips Start() and immediately uses IncreaseSpeed() method
+            if (Speed > 0)
+                State = IVehicle.VehicleState.Moving;
         }
 
-        public void DecreaseSpeed(double change)
+        public void DecreaseSpeed(double changeInMps)
         {
+            var vehicle = (IVehicle) this;
             // if vehicle is on land
             if (CurrentEnvironment == IEnvironment.Environments.LandEnvironment)
             {
                 // is requested speed change results in a speed greater than min land speed
-                if (Speed - change <= IEnvironment.Environments.LandEnvironment.MinSpeed)
+                if (Speed - changeInMps <= CurrentEnvironment.MinSpeed)
                     // reduces speed
-                    Speed -= change;
+                    Speed -= changeInMps;
                 else
                     // else sets speed to min land speed
                     Speed = IEnvironment.Environments.LandEnvironment.MinSpeed;
-
                 // does not go to the landing procedure: vehicle already on land
                 return;
             }
 
             // Landing procedure
             // reduces speed
-            Speed -= change;
-            // if the speed is too high to land => return
-            if ((Speed - change) / 3.6 >= IEnvironment.Environments.AirEnvironment.MinSpeed) return;
+            Speed -= changeInMps;
+            // if the speed is too high to land, return
+            if (Speed - changeInMps >= IEnvironment.Environments.AirEnvironment.MinSpeed)
+                return;
             CurrentEnvironment = IEnvironment.Environments.LandEnvironment; // landing
+            // if requested speed is smaller than land min speed, sets min speed as speed
+            if (Speed < CurrentEnvironment.MinSpeed)
+                Speed = vehicle.GetConvertedSpeed(IEnvironment.SpeedUnit.Kph, IEnvironment.SpeedUnit.Mps,
+                    CurrentEnvironment.MinSpeed);
         }
 
+        // current speed is shown in unit of the current environment
         public override string ToString()
         {
             var vehicle = (IVehicle) this;
-            var unit = CurrentEnvironment.Unit;
             return $"Type: Air Vehicle, Has Engine: {HasEngine}, " +
                    $"Current Environment: {CurrentEnvironment.Type}, " +
-                   $"State: {vehicle.State}, Min Speed: {CurrentEnvironment.MinSpeed} {unit}, " +
-                   $"Max Speed: {CurrentEnvironment.MaxSpeed} {unit}, Speed: {(int) vehicle.GetConvertedSpeed(unit, Speed)} {unit}, " +
+                   $"State: {vehicle.State}, Min Speed: {CurrentEnvironment.MinSpeed} {CurrentEnvironment.Unit}, " +
+                   $"Max Speed: {CurrentEnvironment.MaxSpeed} {CurrentEnvironment.Unit}, " +
+                   $"Speed: {vehicle.GetConvertedSpeed(NativeEnvironment.Unit, CurrentEnvironment.Unit, Speed)} {CurrentEnvironment.Unit}, " +
                    $"Horsepower: {Horsepower}, Fuel Type: {FuelType}";
         }
     }
