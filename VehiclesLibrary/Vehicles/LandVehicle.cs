27db60283eea20e0b29978e5f16926d97929b37f
MattCobosco <mateuszkalucki@gmail.com>
2022-03-26 14:36:49 +0100

    LandVehicle.cs 2.0

--- VehiclesLibrary/Vehicles/LandVehicle.cs
+++ VehiclesLibrary/Vehicles/LandVehicle.cs
@@ -4,68 +4,53 @@ namespace ClassLibrary.Vehicles
 {
     public class LandVehicle : IVehicle
     {
-        // LandVehicle spawns on the ground
-        private readonly IEnvironment CurrentEnvironment = IEnvironment.Environments.LandEnvironment;
-
-        public LandVehicle(int horsepower, bool hasEngine, IVehicle.FuelType fuelType, int wheelCount)
+        // General Vehicle properties
+        // A land vehicle => can't switch environments
+        public IVehicle.VehicleType Type { get; set; } = IVehicle.VehicleType.LandVehicle;
+        public IEnvironment CurrentEnvironment { get; set; } = IEnvironment.Environments.LandEnvironment;
+        public IEnvironment NativeEnvironment { get; set; } = IEnvironment.Environments.LandEnvironment;
+        public double Speed { get; set; }
+        public bool HasEngine { get; set; }
+        public int HorsePower { get; set; }
+        public IVehicle.FuelType Fuel { get; set; }
+        public IVehicle.State VehicleState { get; set; }
+        
+        // LandVehicle-only properties
+        private readonly int _numberOfWheels;
+        
+        public LandVehicle(bool hasEngine, int horsePower, IVehicle.FuelType fuel, int numberOfWheels)
         {
-            Horsepower = horsepower;
             HasEngine = hasEngine;
-            FuelType = fuelType;
-            WheelCount = wheelCount;
-        }
-
-        private double Speed { get; set; }
-        private IVehicle.FuelType FuelType { get; }
-        private int WheelCount { get; }
-
-        public int Horsepower { get; }
-
-        // LandVehicle Spawns as stationary
-        public IVehicle.VehicleState State { get; private set; } = IVehicle.VehicleState.Stationary;
-        public bool HasEngine { get; }
-
-        public void Start()
-        {
-            Speed = CurrentEnvironment.MinSpeed;
-            State = IVehicle.VehicleState.Moving;
-        }
-
-        public void Stop()
-        {
-            Speed = 0;
-            State = IVehicle.VehicleState.Stationary;
-        }
-
-        public void IncreaseSpeed(double changeInKps)
-        {
-            if (Speed + changeInKps <= IEnvironment.Environments.LandEnvironment.MaxSpeed)
-                Speed += changeInKps;
+            if (HasEngine)
+            {
+                HorsePower = horsePower;
+                Fuel = fuel;
+            }
             else
-                Speed = IEnvironment.Environments.LandEnvironment.MaxSpeed;
-
-            if (Speed > 0) State = IVehicle.VehicleState.Moving;
+            {
+                // if no engine => horsepower is 0, fuel type is none
+                HorsePower = 0;
+                Fuel = IVehicle.FuelType.None;
+            }
+            _numberOfWheels = numberOfWheels;
         }
-
-        public void DecreaseSpeed(double changeInKps)
-        {
-            if (Speed - changeInKps >= IEnvironment.Environments.LandEnvironment.MinSpeed)
-                Speed -= changeInKps;
-            else
-                Speed = IEnvironment.Environments.LandEnvironment.MinSpeed;
-
-            if (Speed <= 0) State = IVehicle.VehicleState.Stationary;
-        }
-
+        
         public override string ToString()
         {
             var vehicle = (IVehicle) this;
-            var currentUnit = CurrentEnvironment.Unit;
-            return $"Type: Land Vehicle, Number of wheels: {WheelCount} " +
-                   $"Horsepower: {Horsepower}, Fuel type: {FuelType} " +
+            var nativeUnit = vehicle.NativeEnvironment.Unit;
+            var currentUnit = vehicle.CurrentEnvironment.Unit;
+
+            return $"Data Type: {GetType().Name}, " +
+                   $"Vehicle Type: {vehicle.Type}, " +
                    $"Current Environment: {CurrentEnvironment.Type}, " +
-                   $"State: {vehicle.State}, Min Speed: {CurrentEnvironment.MinSpeed} {currentUnit}, " +
-                   $"Max Speed: {CurrentEnvironment.MaxSpeed} {currentUnit}, Speed: {Speed} {currentUnit}";
+                   $"Current State: {vehicle.VehicleState}, " +
+                   $"Environment Min Speed: {CurrentEnvironment.MinSpeed} {currentUnit}, " +
+                   $"Environment Max Speed: {CurrentEnvironment.MaxSpeed} {currentUnit}, " +
+                   $"Current Speed: {IVehicle.GetConvertedSpeed(nativeUnit, currentUnit, vehicle.Speed)} {nativeUnit}, " +
+                   $"ADDITIONAL: " +
+                   $"HasEngine: {vehicle.HasEngine}, " +
+                   $"Number of Wheels: {_numberOfWheels}";
         }
     }
 }
