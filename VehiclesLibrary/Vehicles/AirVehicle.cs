91e376c017dbbf36aae78e08f404862cabce523c
MattCobosco <mateuszkalucki@gmail.com>
2022-03-26 14:38:07 +0100

    AirVehicle.cs, WaterVehicle.cs 2.0 base

--- VehiclesLibrary/Vehicles/AirVehicle.cs
+++ VehiclesLibrary/Vehicles/AirVehicle.cs
@@ -4,120 +4,13 @@ namespace ClassLibrary.Vehicles
 {
     public class AirVehicle : IVehicle
     {
-        // its default environment is AirEnvironment
-        private static readonly IEnvironment NativeEnvironment = IEnvironment.Environments.AirEnvironment;
-
-        public AirVehicle(int horsepower, bool hasEngine, IVehicle.FuelType fuelType)
-        {
-            Horsepower = horsepower;
-            HasEngine = hasEngine;
-            FuelType = fuelType;
-        }
-
-        // native speed unit for AirVehicle is meters per second, all requested speed changes are made in mps
-        private double Speed { get; set; }
-
-        // AirVehicle spawns on the ground
-        private static IEnvironment CurrentEnvironment { get; set; } = IEnvironment.Environments.LandEnvironment;
-        private IVehicle.FuelType FuelType { get; }
-
-        public int Horsepower { get; }
-
-        // AirVehicle spawns as stationary
-        public IVehicle.VehicleState State { get; private set; } = IVehicle.VehicleState.Stationary;
-        public bool HasEngine { get; }
-
-        public void Start()
-        {
-            var vehicle = (IVehicle) this;
-            // if plane is already moving it cannot be started
-            if (State == IVehicle.VehicleState.Moving) return;
-            Speed = vehicle.GetConvertedSpeed(IEnvironment.SpeedUnit.Kph, IEnvironment.SpeedUnit.Mps,
-                CurrentEnvironment.MinSpeed);
-            State = IVehicle.VehicleState.Moving;
-        }
-
-        public void Stop()
-        {
-            // if plane is in the air it cannot be stopped
-            if (CurrentEnvironment == IEnvironment.Environments.AirEnvironment) return;
-            Speed = 0;
-            State = IVehicle.VehicleState.Stationary;
-        }
-
-        public void IncreaseSpeed(double changeInMps)
-        {
-            // increasing speed in air
-            if (CurrentEnvironment == IEnvironment.Environments.AirEnvironment)
-            {
-                // if the requested speed change is within the max speed limit of the AirEnvironment then it's implemented
-                if (Speed + changeInMps <= IEnvironment.Environments.AirEnvironment.MaxSpeed)
-                {
-                    Speed += changeInMps;
-                    return;
-                }
-
-                // if it exceeds the max limit then the max is the new speed
-                Speed = CurrentEnvironment.MaxSpeed;
-                return;
-            }
-
-            // increasing speed on land
-            Speed += changeInMps;
-
-            if (Speed > IEnvironment.Environments.AirEnvironment.MinSpeed)
-                //takeoff procedure
-                CurrentEnvironment = IEnvironment.Environments.AirEnvironment;
-            // check if requested speed does not exceed the max speed of AirEnvironment
-            if (Speed > CurrentEnvironment.MaxSpeed)
-                //if so, reduce it to max speed
-                Speed = CurrentEnvironment.MaxSpeed;
-            // for situation when air vehicle skips Start() and immediately uses IncreaseSpeed() method
-            if (Speed > 0)
-                State = IVehicle.VehicleState.Moving;
-        }
-
-        public void DecreaseSpeed(double changeInMps)
-        {
-            var vehicle = (IVehicle) this;
-            // if vehicle is on land
-            if (CurrentEnvironment == IEnvironment.Environments.LandEnvironment)
-            {
-                // is requested speed change results in a speed greater than min land speed
-                if (Speed - changeInMps <= CurrentEnvironment.MinSpeed)
-                    // reduces speed
-                    Speed -= changeInMps;
-                else
-                    // else sets speed to min land speed
-                    Speed = IEnvironment.Environments.LandEnvironment.MinSpeed;
-                // does not go to the landing procedure: vehicle already on land
-                return;
-            }
-
-            // Landing procedure
-            // reduces speed
-            Speed -= changeInMps;
-            // if the speed is too high to land, return
-            if (Speed - changeInMps >= IEnvironment.Environments.AirEnvironment.MinSpeed)
-                return;
-            CurrentEnvironment = IEnvironment.Environments.LandEnvironment; // landing
-            if (Speed < CurrentEnvironment.MinSpeed) // check speed: can't be less than land min speed
-                Speed = vehicle.GetConvertedSpeed(IEnvironment.SpeedUnit.Kph, IEnvironment.SpeedUnit.Mps,
-                    CurrentEnvironment.MinSpeed);
-        }
-
-        // current speed is shown in the current environment speed unit
-        public override string ToString()
-        {
-            var vehicle = (IVehicle) this;
-            var currentUnit = CurrentEnvironment.Unit;
-            var nativeUnit = NativeEnvironment.Unit;
-            return $"Type: Air Vehicle, Has Engine: {HasEngine}, " +
-                   $"Current Environment: {CurrentEnvironment.Type}, " +
-                   $"State: {vehicle.State}, Min Speed: {CurrentEnvironment.MinSpeed} {currentUnit}, " +
-                   $"Max Speed: {CurrentEnvironment.MaxSpeed} {currentUnit}, " +
-                   $"Speed: {vehicle.GetConvertedSpeed(nativeUnit, currentUnit, Speed)} {currentUnit}, " +
-                   $"Horsepower: {Horsepower}, Fuel Type: {FuelType}";
-        }
+        public IVehicle.VehicleType Type { get; set; } = IVehicle.VehicleType.AirVehicle;
+        public IEnvironment CurrentEnvironment { get; set; }
+        public IEnvironment NativeEnvironment { get; set; }
+        public double Speed { get; set; }
+        public bool HasEngine { get; set; }
+        public int HorsePower { get; set; }
+        public IVehicle.FuelType Fuel { get; set; }
+        public IVehicle.State VehicleState { get; set; }
     }
 }
--- VehiclesLibrary/Vehicles/WaterVehicle.cs
+++ VehiclesLibrary/Vehicles/WaterVehicle.cs
@@ -5,107 +5,13 @@ namespace ClassLibrary.Vehicles
 {
     public class WaterVehicle : IVehicle
     {
-        // TODO: Test WaterVehicle in console and fix potential issues
-        public WaterVehicle(int horsepower, int displacement, bool hasEngine, bool? amphibious, int? wheelCount, IEnvironment? currentEnvironment)
-        {
-            Horsepower = horsepower;
-            HasEngine = hasEngine;
-            // engine is always a diesel engine
-            if(HasEngine)
-                _fuelType = IVehicle.FuelType.Diesel;
-            _displacement = displacement;
-            // spawns stationary
-            State = IVehicle.VehicleState.Stationary;
-            // can be amphibious
-            // if amphibious is null then the vehicle is not amphibious
-            _amphibious = amphibious ?? false;
-            // can have wheels if amphibious 
-            _wheelCount = wheelCount ?? 0;
-            // if currentEnvironment is null then vehicle spawns in water
-            CurrentEnvironment = currentEnvironment ?? IEnvironment.Environments.WaterEnvironment;
-        }
-
-        private double Speed { get; set; }
-        public IVehicle.VehicleState State { get; private set; }
-        public bool HasEngine { get; }
-        public int Horsepower { get; }
-        private readonly IVehicle.FuelType _fuelType;
-        private readonly int _displacement;
-        private IEnvironment CurrentEnvironment { get; set; }
-        private readonly bool _amphibious;
-        private readonly int _wheelCount;
-
-        public void Start()
-        {
-            if (State != IVehicle.VehicleState.Moving) return;
-            Speed = CurrentEnvironment.MinSpeed;
-            State = IVehicle.VehicleState.Moving;
-        }
-
-        public void Stop()
-        {
-            if (State != IVehicle.VehicleState.Stationary) return;
-            Speed = 0;
-            State = IVehicle.VehicleState.Stationary;
-        }
-
-        public void IncreaseSpeed(double change)
-        {
-            if (Speed + change <= CurrentEnvironment.MaxSpeed)
-            {
-                Speed += change;
-            }
-            else
-            {
-                Speed = CurrentEnvironment.MaxSpeed;
-            }
-
-            if (Speed > 0)
-            {
-                State = IVehicle.VehicleState.Moving;
-            }
-        }
-
-        public void DecreaseSpeed(double change)
-        {
-            if (Speed - change >= CurrentEnvironment.MinSpeed)
-            {
-                Speed -= change;
-            }
-            else
-            {
-                Speed = CurrentEnvironment.MinSpeed;
-            }
-        }
-        
-        // Change between Water and Land Environments
-        public void ChangeEnvironment()
-        {
-            // non-amphibious vehicle cannot switch environment
-            if (!_amphibious) return;
-            if (IVehicle.CurrentEnvironment == IEnvironment.Environments.LandEnvironment)
-            {
-                CurrentEnvironment = IEnvironment.Environments.WaterEnvironment;
-                Speed *= 0.54;
-            }
-            else
-            {
-                CurrentEnvironment = IEnvironment.Environments.LandEnvironment;
-            }
-        }
-
-
-        public override string ToString()
-        {
-            var vehicle = (IVehicle) this;
-            var unit = CurrentEnvironment.Unit;
-            var firstPart = _amphibious ? $"Type: Amphibious Vehicle, Number of wheels: {_wheelCount}" : "Type: Water Vehicle,";
-            
-            return $"{firstPart} " +
-                   $"Displacement {_displacement}, Horsepower: {Horsepower}, " +
-                   $"Has Engine: {HasEngine}, Fuel Type: {_fuelType}, " +
-                   $"Current Environment {CurrentEnvironment.Type}, Min Speed: {CurrentEnvironment.MinSpeed} {unit}, " +
-                   $"Max Speed: {CurrentEnvironment.MaxSpeed} {unit}, Speed: {vehicle.GetConvertedSpeed(unit, Speed)}";
-        }
+        public IVehicle.VehicleType Type { get; set; }
+        public IEnvironment CurrentEnvironment { get; set; }
+        public IEnvironment NativeEnvironment { get; set; }
+        public double Speed { get; set; }
+        public bool HasEngine { get; set; }
+        public int HorsePower { get; set; }
+        public IVehicle.FuelType Fuel { get; set; }
+        public IVehicle.State VehicleState { get; set; }
     }
 }
