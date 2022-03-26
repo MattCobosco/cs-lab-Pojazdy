3957bd345e4b5377ea1f80c5d414575a4ad54aef
MattCobosco <mateuszkalucki@gmail.com>
2022-03-27 00:19:01 +0100

    some work

--- VehiclesLibrary/Vehicles/WaterVehicle.cs
+++ VehiclesLibrary/Vehicles/WaterVehicle.cs
@@ -7,13 +7,13 @@ namespace ClassLibrary.Vehicles
     // TODO: Finish WaterVehicle class
     public class WaterVehicle : IVehicle
     {
-        public IVehicle.VehicleType Type { get; set; }
-        public IEnvironment CurrentEnvironment { get; set; }
-        public IEnvironment NativeEnvironment { get; set; }
+        public IVehicle.VehicleType Type { get; set; } = IVehicle.VehicleType.WaterVehicle;
+        public IEnvironment CurrentEnvironment { get; set; } = IEnvironment.Environments.WaterEnvironment;
+        public IEnvironment NativeEnvironment { get; set; } = IEnvironment.Environments.WaterEnvironment;
         public double Speed { get; set; }
         public bool HasEngine { get; set; }
         public int HorsePower { get; set; }
-        public IVehicle.FuelType Fuel { get; set; }
+        public IVehicle.FuelType FuelUsed { get; set; }
         public IVehicle.State VehicleState { get; set; }
     }
 }
