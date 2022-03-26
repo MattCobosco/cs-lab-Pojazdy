e70563453b66d5a2a80527678c5600c56b6d7e29
MattCobosco <mateuszkalucki@gmail.com>
2022-03-27 00:18:27 +0100

    LandVehicle.cs improvements: new comments, code cleanup

--- VehiclesLibrary/Vehicles/LandVehicle.cs
+++ VehiclesLibrary/Vehicles/LandVehicle.cs
@@ -4,40 +4,48 @@ namespace ClassLibrary.Vehicles
 {
     public class LandVehicle : IVehicle
     {
-        // General Vehicle properties
-        // A land vehicle => can't switch environments
-        public IVehicle.VehicleType Type { get; set; } = IVehicle.VehicleType.LandVehicle;
-        public IEnvironment CurrentEnvironment { get; set; } = IEnvironment.Environments.LandEnvironment;
-        public IEnvironment NativeEnvironment { get; set; } = IEnvironment.Environments.LandEnvironment;
-        public double Speed { get; set; }
-        public bool HasEngine { get; set; }
-        public int HorsePower { get; set; }
-        public IVehicle.FuelType Fuel { get; set; }
-        public IVehicle.State VehicleState { get; set; }
-        
-        // LandVehicle-only properties
+        // Land vehicle-only properties
         private readonly int _numberOfWheels;
-        
-        public LandVehicle(bool hasEngine, int horsePower, IVehicle.FuelType fuel, int numberOfWheels)
+
+        public LandVehicle(bool hasEngine, int horsePower, IVehicle.FuelType fuelType, int numberOfWheels)
         {
             HasEngine = hasEngine;
             if (HasEngine)
             {
                 HorsePower = horsePower;
-                Fuel = fuel;
+                FuelUsed = fuelType;
             }
             else
             {
-                // if no engine => horsepower is 0, fuel type is none
+                // If no engine => horsepower is 0, fuel type is none
                 HorsePower = 0;
-                Fuel = IVehicle.FuelType.None;
+                FuelUsed = IVehicle.FuelType.None;
             }
+
             _numberOfWheels = numberOfWheels;
         }
-        
+
+        // General vehicle properties
+        // A land vehicle => cannot switch environments
+        public IVehicle.VehicleType Type { get; set; } = IVehicle.VehicleType.LandVehicle;
+
+        // Spawns on land
+        public IEnvironment CurrentEnvironment { get; set; } = IEnvironment.Environments.LandEnvironment;
+
+        // Native environment is land => the unit for speed and its changes will be kph
+        public IEnvironment NativeEnvironment { get; set; } = IEnvironment.Environments.LandEnvironment;
+        public double Speed { get; set; }
+        public bool HasEngine { get; set; }
+        public int HorsePower { get; set; }
+
+        public IVehicle.FuelType FuelUsed { get; set; }
+
+        // Spawns stationary
+        public IVehicle.State VehicleState { get; set; } = IVehicle.State.Stationary;
+
         public override string ToString()
         {
-            var vehicle = (IVehicle) this;
+            IVehicle vehicle = this;
             var nativeUnit = vehicle.NativeEnvironment.Unit;
             var currentUnit = vehicle.CurrentEnvironment.Unit;
 
@@ -47,9 +55,11 @@ namespace ClassLibrary.Vehicles
                    $"Current State: {vehicle.VehicleState}, " +
                    $"Environment Min Speed: {CurrentEnvironment.MinSpeed} {currentUnit}, " +
                    $"Environment Max Speed: {CurrentEnvironment.MaxSpeed} {currentUnit}, " +
-                   $"Current Speed: {IVehicle.GetConvertedSpeed(nativeUnit, currentUnit, vehicle.Speed)} {nativeUnit}, " +
-                   $"ADDITIONAL: " +
+                   $"Current Speed: {IVehicle.GetConvertedSpeed(nativeUnit, currentUnit, vehicle.Speed):0.00} {currentUnit}, " +
+                   "ADDITIONAL: " +
                    $"HasEngine: {vehicle.HasEngine}, " +
+                   $"Horsepower: {vehicle.HorsePower}, " +
+                   $"Type of Fuel: {vehicle.FuelUsed}, " +
                    $"Number of Wheels: {_numberOfWheels}";
         }
     }
