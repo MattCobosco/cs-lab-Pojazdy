7184a42d32b6c08df350741003eb6b7d91f66de2
MattCobosco <mateuszkalucki@gmail.com>
2022-03-27 18:23:45 +0200

    initial commit

---
+++ cs-lab-Pojazdy/Vehicles/Car.cs
@@ -0,0 +1,10 @@
+﻿using ClassLibrary.Vehicles;
+
+namespace Vehicles;
+
+public class Car : LandVehicle, IVehicle
+{
+    public Car(bool hasEngine, int horsePower, IVehicle.FuelType fuelType, int numberOfWheels) : base(hasEngine, horsePower, fuelType, numberOfWheels)
+    {
+    }
+}
