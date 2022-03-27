6556fe024e300a82d6817d530d6ef0a2c36251b4
MattCobosco <mateuszkalucki@gmail.com>
2022-03-27 18:25:49 +0200

    WaterVehicle.cs 2.0

--- VehiclesLibrary/Vehicles/WaterVehicle.cs
+++ VehiclesLibrary/Vehicles/WaterVehicle.cs
@@ -1,5 +1,4 @@
 #nullable enable
-using System.Xml.Schema;
 using ClassLibrary.Environments;
 
 namespace ClassLibrary.Vehicles
@@ -7,13 +6,58 @@ namespace ClassLibrary.Vehicles
     // TODO: Finish WaterVehicle class
     public class WaterVehicle : IVehicle
     {
+        // Water vehicle-specific properties
+        private readonly int _displacement;
+
+        // General vehicle properties
+        // A water vehicle => cannot switch environments
         public IVehicle.VehicleType Type { get; set; } = IVehicle.VehicleType.WaterVehicle;
+        // Spawns on water
         public IEnvironment CurrentEnvironment { get; set; } = IEnvironment.Environments.WaterEnvironment;
+        // Native environment is water => the unit for speed and speed changes is knots
         public IEnvironment NativeEnvironment { get; set; } = IEnvironment.Environments.WaterEnvironment;
         public double Speed { get; set; }
         public bool HasEngine { get; set; }
         public int HorsePower { get; set; }
         public IVehicle.FuelType FuelUsed { get; set; }
-        public IVehicle.State VehicleState { get; set; }
+        // Spawns stationary
+        public IVehicle.State VehicleState { get; set; } = IVehicle.State.Stationary;
+
+        public WaterVehicle(bool hasEngine, int horsePower, int displacement)
+        {
+            HasEngine = hasEngine;
+            if (hasEngine)
+            {
+                HorsePower = horsePower;
+                FuelUsed = IVehicle.FuelType.Diesel;
+            }
+            else
+            {
+                HorsePower = 0;
+                FuelUsed = IVehicle.FuelType.None;
+            }
+
+            _displacement = displacement;
+        }
+
+        public override string ToString()
+        {
+            IVehicle vehicle = this;
+            var nativeUnit = vehicle.NativeEnvironment.Unit;
+            var currentUnit = vehicle.CurrentEnvironment.Unit;
+
+            return $"Data Type: {GetType().Name}, " +
+                   $"Vehicle Type: {vehicle.Type}, " +
+                   $"Current Environment: {CurrentEnvironment.Type}, " +
+                   $"Current State: {vehicle.VehicleState}, " +
+                   $"Environment Min Speed: {CurrentEnvironment.MinSpeed} {currentUnit}, " +
+                   $"Environment Max Speed: {CurrentEnvironment.MaxSpeed} {currentUnit}, " +
+                   $"Current Speed: {IVehicle.GetConvertedSpeed(nativeUnit, currentUnit, vehicle.Speed):0.00} {currentUnit}, " +
+                   "ADDITIONAL: " +
+                   $"HasEngine: {vehicle.HasEngine}, " +
+                   $"Horsepower: {vehicle.HorsePower}, " +
+                   $"Type of Fuel: {vehicle.FuelUsed}, " +
+                   $"Displacement: {_displacement}";
+        }
     }
 }
